d31d7fa3c0583a8b36e9bceb2f1605fda83f6984
AndytGomez <andrew.tocora@uniminuto.edu.co>
2026-03-13 23:20:47 -0500

    Add script to calculate average of three notes

---
+++ SEMANA3/PROMEDIO.py
@@ -0,0 +1,6 @@
+#GENERE UN PROMEDIO DE NOTAS
+NM1=float(input("Write the first note"))
+NM2=float(input("Write the second note"))
+NM3=float(input("Write the third note"))
+P=(NM1+NM2+NM3)/3
+print("The average of the three notes is",P)
