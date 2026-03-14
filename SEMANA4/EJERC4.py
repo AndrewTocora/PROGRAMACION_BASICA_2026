c9022e596334e27e8250801b3b209e2343ccd1f8
AndytGomez <andrew.tocora@uniminuto.edu.co>
2026-03-13 23:52:44 -0500

    Create EJERC4.py

---
+++ SEMANA4/EJERC4.py
@@ -0,0 +1,26 @@
+#GENERE UN PROMEDIO DE NOTAS
+#ALGORITMO
+# Pedir la NM1
+# Pedir la NM2
+# Pedir la NM3
+# Mostrar NM1
+# Mostrar NM2
+# Mostrar NM3
+# Calcular el promedio (NM1+NM2+NM3)/3
+# Promedio 
+# Comparar si aprobó (Nota minima 3.0) 
+
+#PSEUDOCODIGO 
+#NM1=float(input("Write the first note"))
+#NM2=float(input("Write the second note"))
+#NM3=float(input("Write the third note"))
+#P=NM1+NM2+NM3)/3
+#print("The average of the three notes is",P)
+#print("¿The studen aprobe?: ", P >= 3.0)
+
+NM1=float(input("Write the first note"))
+NM2=float(input("Write the second note"))
+NM3=float(input("Write the third note"))
+P=(NM1+NM2+NM3)/3
+print("The average of the three notes is",P)
+print("¿The studen aprobe?: ", P >= 3.0)
