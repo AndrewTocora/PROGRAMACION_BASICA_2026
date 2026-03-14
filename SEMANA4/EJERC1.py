dcc69d38e2aef3773aba73fddce3338d239e0766
AndytGomez <andrew.tocora@uniminuto.edu.co>
2026-03-13 23:35:38 -0500

    Implement basic user information input and output

---
+++ SEMANA4/EJERC1.py
@@ -0,0 +1,26 @@
+#ALGORITMO 
+#1) Pedir el nombre 
+#2) Pedir la edad 
+#3) Pedir la ciudad 
+#4) Mostrar el nombre 
+#5) Mostrar la edad
+#6) Mostrar la ciudad
+#7) Comparar si la edad es mayor o igual a 18
+#8) Mostrar el resultado
+
+#PSEUDOCODIGO  
+    #LEER Nombre
+    #LEER Años
+    #LEER Ciudad
+    #PRINT "Hola, mi nombre es", nombre
+    #PRINT "Tengo", edad, "años"
+    #PRINT "Soy de", ciudad 
+    #PRINT "Es mayor de edad", edad>18
+
+Nombre = input("Escribe tu nombre: ")
+Años = int (input("Escribe tu edad: "))
+Ciudad = input("¿Donde vives?: ")
+print("Hola, me llamo", Name)
+print("Tengo ", Age, "años")
+print("Soy de ", City)
+print("¿Eres mayor de edad?", Age>18)
