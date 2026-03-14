17fca02771213cbfc731bc50575c138503f5a5a4
AndytGomez <andrew.tocora@uniminuto.edu.co>
2026-03-13 23:24:51 -0500

    Add basic arithmetic operations and comparisons

---
+++ SEMANA3/EJERCICIO1.py
@@ -0,0 +1,17 @@
+#Esto es un dato float
+a=40
+#Esto es un dato entero
+b=150
+print("El resultado de suma es:", a+b)
+print("El resultado de resta es:",a-b)
+print("El resultado de multiplicacion es:",a*b)
+#Esta division es decimal
+print("El resultado de division es:",a/b)
+#Esta divison es entera
+print("El resultado de esta division entera es:",a//b)
+#El reciduo de la division
+print("El resultado del residuo es:",a%b)
+#Comparaciones
+print(a<b)
+print(a>b)
+print(a==b)
