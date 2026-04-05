33b17bfeb0b4d6b5ab6b7e32581fd8c88fa51bcf
AndytGomez <andrew.tocora@uniminuto.edu.co>
2026-04-05 09:45:41 -0500

    Add driving permit check based on age and status

---
+++ SEMANA7/PASECONDUCION.py
@@ -0,0 +1,6 @@
+E=int(input("Ingresa tu edad: "))
+C = input("¿Tienes pase vigente?(si/no):")
+if E >=18  and C == "si":
+    print ("Puedes continuar con su viaje. Maneje con precauciòn")
+else:
+    print ("El vehiculo sera retenido y usted sera llevado al CAI.")
